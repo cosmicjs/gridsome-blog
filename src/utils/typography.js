a19dbede94d4edee7bc9e2ec302053f7530100cb
Jazib Sawar <jazibsawar@gmail.com>
2019-01-24 01:15:28 +0500

    Typography error

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -12,9 +12,7 @@ delete Wordpress2016.googleFonts
 const typography = new Typography(Wordpress2016)
 
 // Hot reload typography in development.
-if (process.env.NODE_ENV !== 'production') {
-  typography.injectStyles()
-}
+typography.injectStyles()
 
 export default typography
 export const rhythm = typography.rhythm
